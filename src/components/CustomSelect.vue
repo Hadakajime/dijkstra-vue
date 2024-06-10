4c3969aceaa86d4d26ed7731d9c891678bc0650d
Jason Zuvela <jasonzuvela@gmail.com>
2024-06-10 20:40:44 +1000

    Convert more code.

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+import type { OptionType } from "@/types";
+
+export type CustomSelectProps = {
+	options: OptionType[];
+	value?: OptionType | null;
+	placeholder?: string;
+	label?: string;
+	disabled?: boolean;
+	id?: string;
+	onChange: (selectedOption: OptionType) => void;
+};
+
+export default defineComponent({
+	name: "CustomSelect",
+	props: {
+		options: {
+			type: Array as PropType<CustomSelectProps["options"]>,
+		},
+	},
+});
+</script>
