9951e70d3820d7ab5a6214afecc2e900775d14cd
Jason Zuvela <jasonzuvela@gmail.com>
2024-06-14 23:53:13 +1000

    Move node selection to dijkstra store.

--- src/components/CalculatorCard.vue
+++ src/components/CalculatorCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="calculator-card mt-[-175px] flex flex-col justify-center items-center">
 		<div class="toggle-wrapper flex relative bg-white rounded-full px-[12px] py-[8px] max-w-[270px] justify-center mb-[24px]">
-			<Toggle v-model="isRandomMode" :hasRefreshIcon="hasRefreshIcon" :refreshClick="fetchRandomRefreshHandler">Enable Random Mode</Toggle>
+			<Toggle v-model="isRandomMode" :hasRefreshIcon="hasRefreshIcon" :refreshClick="fetchRandomNumbers">Enable Random Mode</Toggle>
 		</div>
 		<div class="w-[721px] bg-white rounded-[8px] shadow-md flex flex-col md:w-[400px]">
 			<div class="calculator-card-inner">
@@ -34,26 +34,14 @@
 
 <script lang="ts">
 import { defineComponent, defineAsyncComponent } from "vue";
-import { mapWritableState, mapActions } from "pinia";
+import { mapActions, mapState } from "pinia";
 import { useDijkstraStore } from "@stores/dijkstraStore";
 import type { OptionType } from "@defs/index";
 import { SELECT_OPTIONS } from "@constants/index";
-import { getRandomNumbers } from "@utils/getRandomNumbers";
 import handleSendResult from "@utils/SendShortestPathData";
 import { dijkstra } from "@utils/dijkstra";
 
 type Data = {
-	fromSelectedOption: OptionType | null;
-	toSelectedOption: OptionType | null;
-	isInputValidationErr: boolean;
-	isAppSuccess: boolean;
-	isAppError: boolean;
-	isAppDefault: boolean;
-	isAppLoading: boolean;
-	isSelectDisabled: boolean;
-	isClearBtnDisabled: boolean;
-	hasRefreshIcon: boolean;
-	isCalculateBtnDisabled: boolean;
 	resultResStatus: number;
 	resultNodeNames: string[];
 	resultDistance: number;
@@ -71,50 +59,35 @@ export default defineComponent({
 		Toggle: defineAsyncComponent(() => import("@components/Toggle.vue")),
 	},
 	data: (): Data => ({
-		fromSelectedOption: null,
-		toSelectedOption: null,
-		isInputValidationErr: false,
-		isAppSuccess: false,
-		isAppError: false,
-		isAppDefault: true,
-		isAppLoading: false,
-		isSelectDisabled: false,
-		isClearBtnDisabled: false,
-		hasRefreshIcon: false,
-		isCalculateBtnDisabled: false,
 		resultResStatus: -1,
 		resultNodeNames: [],
 		resultDistance: -1,
 	}),
 	computed: {
-		...mapWritableState(useDijkstraStore, [
+		...mapState(useDijkstraStore, [
 			"activeMode",
 			"fromNode",
 			"graphState",
 			"shortestPathData",
 			"toNode",
+			"hasRefreshIcon",
+			"isAppDefault",
+			"isAppError",
+			"isAppLoading",
+			"isAppSuccess",
+			"isCalculateBtnDisabled",
+			"isClearBtnDisabled",
+			"isInputValidationErr",
+			"isSelectDisabled",
 		]),
 		isRandomMode: {
 			get() {
 				return this.activeMode === "random";
 			},
 			set(value: boolean) {
-				this.isAppSuccess = false;
-				this.isAppError = false;
-				this.isAppLoading = false;
-				this.isAppDefault = true;
-				this.isInputValidationErr = false;
-				this.isCalculateBtnDisabled = false;
 				if (value) {
-					this.isSelectDisabled = true;
-					this.isClearBtnDisabled = true;
-					this.hasRefreshIcon = true;
 					this.setMode("random");
-					this.fetchRandomNumberHandler();
 				} else {
-					this.isSelectDisabled = false;
-					this.isClearBtnDisabled = false;
-					this.hasRefreshIcon = false;
 					this.setMode("input");
 				}
 			},
@@ -122,68 +95,40 @@ export default defineComponent({
 		selectOptions() {
 			return SELECT_OPTIONS;
 		},
+		fromSelectedOption: {
+			get(): OptionType {
+				return {
+					label: this.fromNode,
+					value: this.fromNode,
+				};
+			},
+			set(value: OptionType) {
+				this.updateNodeSelection(value.value, this.toNode);
+			},
+		},
+		toSelectedOption: {
+			get(): OptionType {
+				return {
+					label: this.toNode,
+					value: this.toNode,
+				};
+			},
+			set(value: OptionType) {
+				this.updateNodeSelection(this.fromNode, value.value);
+			},
+		},
 	},
 	methods: {
 		...mapActions(useDijkstraStore, [
 			"addGraphVertex",
 			"addGraphEdge",
 			"setMode",
+			"updateNodeSelection",
+			"initialiseDefaultGraph",
+			"fetchRandomNumbers",
 		]),
-		async fetchRandomNumberHandler() {
-			if (this.activeMode === "random") {
-				try {
-					this.isCalculateBtnDisabled = true;
-					this.isAppDefault = false;
-					this.isAppLoading = true;
-					const randomLetters = await getRandomNumbers();
-					this.fromNode = randomLetters?.fromNode;
-					this.toNode = randomLetters?.toNode;
-					this.fromSelectedOption = {
-						value: randomLetters?.fromNode,
-						label: randomLetters?.fromNode,
-					};
-					this.toSelectedOption = {
-						value: randomLetters?.toNode,
-						label: randomLetters?.toNode,
-					};
-				} catch (error) {
-					console.error("Error:", error);
-				}
-			}
-			this.isCalculateBtnDisabled = false;
-			this.isAppLoading = false;
-			this.isAppDefault = true;
-		},
-		fetchRandomRefreshHandler() {
-			this.isAppDefault = true;
-			this.isAppSuccess = false;
-			this.fetchRandomNumberHandler();
-		},
-		handleFromChange(newValue: OptionType) {
-			this.isCalculateBtnDisabled = false;
-			this.isAppDefault = true;
-			this.isAppSuccess = false;
-			this.fromSelectedOption = newValue;
-			this.fromNode = newValue?.value;
-		},
-		handleToChange(newValue: OptionType) {
-			this.isCalculateBtnDisabled = false;
-			this.isAppDefault = true;
-			this.isAppSuccess = false;
-			this.toSelectedOption = newValue;
-			this.toNode = newValue?.value;
-		},
 		clearBtnHandler() {
-			this.isAppSuccess = false;
-			this.isAppError = false;
-			this.isAppLoading = false;
-			this.isAppDefault = true;
-			this.isInputValidationErr = false;
-			this.isCalculateBtnDisabled = false;
-			this.fromSelectedOption = null;
-			this.toSelectedOption = null;
-			this.fromNode = "";
-			this.toNode = "";
+			this.updateNodeSelection("", "");
 		},
 		calculateHandler() {
 			if (this.fromNode?.trim() !== "" && this.toNode?.trim() !== "") {
@@ -219,40 +164,7 @@ export default defineComponent({
 		},
 	},
 	mounted() {
-		this.addGraphVertex(0, "A");
-		this.addGraphVertex(1, "B");
-		this.addGraphVertex(2, "C");
-		this.addGraphVertex(3, "D");
-		this.addGraphVertex(4, "E");
-		this.addGraphVertex(5, "F");
-		this.addGraphVertex(6, "G");
-		this.addGraphVertex(7, "H");
-		this.addGraphVertex(8, "I");
-		this.addGraphEdge("A", "B", 4);
-		this.addGraphEdge("A", "C", 6);
-		this.addGraphEdge("B", "A", 4);
-		this.addGraphEdge("B", "F", 2);
-		this.addGraphEdge("C", "A", 6);
-		this.addGraphEdge("C", "D", 8);
-		this.addGraphEdge("D", "C", 8);
-		this.addGraphEdge("D", "E", 4);
-		this.addGraphEdge("D", "G", 1);
-		this.addGraphEdge("E", "B", 2);
-		this.addGraphEdge("E", "F", 3);
-		this.addGraphEdge("E", "I", 8);
-		this.addGraphEdge("E", "D", 4);
-		this.addGraphEdge("F", "B", 2);
-		this.addGraphEdge("F", "E", 3);
-		this.addGraphEdge("F", "G", 4);
-		this.addGraphEdge("F", "H", 6);
-		this.addGraphEdge("G", "D", 1);
-		this.addGraphEdge("G", "F", 4);
-		this.addGraphEdge("G", "H", 5);
-		this.addGraphEdge("G", "I", 5);
-		this.addGraphEdge("H", "F", 6);
-		this.addGraphEdge("H", "G", 5);
-		this.addGraphEdge("I", "E", 8);
-		this.addGraphEdge("I", "G", 5);
+		this.initialiseDefaultGraph();
 	},
 });
 </script>
--- src/stores/dijkstraStore.ts
+++ src/stores/dijkstraStore.ts
@@ -1,37 +1,142 @@
 import { defineStore } from "pinia";
+import { getRandomNumbers } from "@utils/getRandomNumbers";
 import type { GraphState, ShortestPathData } from "@defs/index";
 
 export type DijkstraStoreState = {
 	activeMode: "input" | "random";
 	fromNode: string;
-	toNode: string;
-	shortestPathData?: ShortestPathData;
 	graphState: GraphState;
+	hasRefreshIcon: boolean;
+	isAppDefault: boolean;
+	isAppError: boolean;
+	isAppLoading: boolean;
+	isAppSuccess: boolean;
+	isCalculateBtnDisabled: boolean;
+	isClearBtnDisabled: boolean;
+	isInputValidationErr: boolean;
+	isSelectDisabled: boolean;
+	shortestPathData?: ShortestPathData;
+	toNode: string;
 };
 
 export const useDijkstraStore = defineStore("dijkstra", {
 	state: (): DijkstraStoreState => ({
 		activeMode: "input",
 		fromNode: "",
-		toNode: "",
-		shortestPathData: undefined,
 		graphState: {
 			adjacencyList: {},
 			vertexData: [],
 		},
+		hasRefreshIcon: false,
+		isAppDefault: true,
+		isAppError: false,
+		isAppLoading: false,
+		isAppSuccess: false,
+		isCalculateBtnDisabled: false,
+		isClearBtnDisabled: false,
+		isInputValidationErr: false,
+		isSelectDisabled: false,
+		shortestPathData: undefined,
+		toNode: "",
 	}),
 	actions: {
 		addGraphVertex(vertex: number, data: string) {
-			this.$state.graphState.vertexData[vertex] = data;
+			this.graphState.vertexData[vertex] = data;
 		},
 		addGraphEdge(u: string, v: string, weight: number) {
-			if (!this.$state.graphState.adjacencyList[u]) {
-				this.$state.graphState.adjacencyList[u] = {};
+			if (!this.graphState.adjacencyList[u]) {
+				this.graphState.adjacencyList[u] = {};
 			}
-			this.$state.graphState.adjacencyList[u][v] = weight;
+			this.graphState.adjacencyList[u][v] = weight;
 		},
 		setMode(mode: DijkstraStoreState["activeMode"]) {
-			this.$state.activeMode = mode;
+			this.activeMode = mode;
+			this.isAppSuccess = false;
+			this.isAppError = false;
+			this.isAppLoading = false;
+			this.isAppDefault = true;
+			this.isInputValidationErr = false;
+			this.isCalculateBtnDisabled = false;
+			switch (mode) {
+				case "random":
+					this.isSelectDisabled = true;
+					this.isClearBtnDisabled = true;
+					this.hasRefreshIcon = true;
+					this.fetchRandomNumbers(); // no await
+					break;
+				case "input":
+					this.isSelectDisabled = false;
+					this.isClearBtnDisabled = false;
+					this.hasRefreshIcon = false;
+					break;
+			}
+		},
+		updateNodeSelection(fromNode: string, toNode: string) {
+			this.fromNode = fromNode;
+			this.toNode = toNode;
+			this.isCalculateBtnDisabled = false;
+			this.isAppDefault = true;
+			this.isAppSuccess = false;
+			this.isAppError = false;
+			this.isAppLoading = false;
+			this.isInputValidationErr = false;
+		},
+		async fetchRandomNumbers() {
+			if (this.activeMode !== "random") {
+				return;
+			}
+			this.isCalculateBtnDisabled = true;
+			this.isAppDefault = false;
+			this.isAppLoading = true;
+			try {
+				const randomLetters = await getRandomNumbers();
+				this.updateNodeSelection(randomLetters?.fromNode, randomLetters?.toNode);
+			} catch (error) {
+				console.error("Error:", error);
+			}
+			this.isCalculateBtnDisabled = false;
+			this.isAppLoading = false;
+			this.isAppDefault = true;
+		},
+		initialiseDefaultGraph() {
+			this.graphState = {
+				adjacencyList: {},
+				vertexData: [],
+			};
+			this.addGraphVertex(0, "A");
+			this.addGraphVertex(1, "B");
+			this.addGraphVertex(2, "C");
+			this.addGraphVertex(3, "D");
+			this.addGraphVertex(4, "E");
+			this.addGraphVertex(5, "F");
+			this.addGraphVertex(6, "G");
+			this.addGraphVertex(7, "H");
+			this.addGraphVertex(8, "I");
+			this.addGraphEdge("A", "B", 4);
+			this.addGraphEdge("A", "C", 6);
+			this.addGraphEdge("B", "A", 4);
+			this.addGraphEdge("B", "F", 2);
+			this.addGraphEdge("C", "A", 6);
+			this.addGraphEdge("C", "D", 8);
+			this.addGraphEdge("D", "C", 8);
+			this.addGraphEdge("D", "E", 4);
+			this.addGraphEdge("D", "G", 1);
+			this.addGraphEdge("E", "B", 2);
+			this.addGraphEdge("E", "F", 3);
+			this.addGraphEdge("E", "I", 8);
+			this.addGraphEdge("E", "D", 4);
+			this.addGraphEdge("F", "B", 2);
+			this.addGraphEdge("F", "E", 3);
+			this.addGraphEdge("F", "G", 4);
+			this.addGraphEdge("F", "H", 6);
+			this.addGraphEdge("G", "D", 1);
+			this.addGraphEdge("G", "F", 4);
+			this.addGraphEdge("G", "H", 5);
+			this.addGraphEdge("G", "I", 5);
+			this.addGraphEdge("H", "F", 6);
+			this.addGraphEdge("H", "G", 5);
+			this.addGraphEdge("I", "E", 8);
+			this.addGraphEdge("I", "G", 5);
 		},
 	},
 });
